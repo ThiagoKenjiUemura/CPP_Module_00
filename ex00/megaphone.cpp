6d76b3f7be2e4b1194c6637de581c7fac6da401b
Thiago Kenji Uemura <tkenji-u@c1r1p3.42sp.org.br>
2026-03-10 19:49:05 -0300

    ex00 made

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+	if(argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 1;
+	while(argv[i])
+	{
+		j = 0;
+		while(argv[i][j])
+		{
+			if(argv[i][j] >= 97 && argv[i][j] <= 122)
+				argv[i][j] -= 32;
+			std::cout << argv[i][j];
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
